e26ce7e7446037ae4f2a24b294333d1108233714
akazad1235 <akazad914@gmail.com>
2020-03-25 00:22:48 +0600

    bostrap link added

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+	
+
+</head>
+<body>
+
+	<div class="btn btn-info">button</div>
+	
+	
+	<script src="{{asset('js/jquery-3.4.1.slim.min.js')}}"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('js/popper.min.js')}}"></script>
+</body>
+</html>
