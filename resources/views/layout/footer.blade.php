8103a07665952f636bb9b43919b0573a318531c2
akazad1235 <akazad914@gmail.com>
2020-03-25 14:50:40 +0600

    service img & footer added

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,55 @@
+
+	<div class="container-fluid ">
+		<div class="row ">
+			<div class="col-md-3">
+				<h3>Follow Me</h3>
+				<ul>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+				</ul>
+			</div>
+		
+
+			<div class="col-md-3">
+				<h3>My Address</h3>
+				<ul>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-3">
+				<h3>Information</h3>
+				<ul>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h3>Legal</h3>
+				<ul>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+					<li><a href="">link</a></li>
+				</ul>
+			</div>
+		</div>
+
+		
+
+	</div>	
+
+	<div class="container-fluid bg-dark text-center text-white p-3">
+		<h6>All Rights Reserved By Azad Hosen © 2020-2021 </h6>
+	</div>
